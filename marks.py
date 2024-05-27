f59948dc217b9abf57a561fbc2a155caa97f3243
kareemulla04 <164545582+kareemulla04@users.noreply.github.com>
2024-05-27 11:01:27 +0530

    Add files via upload

---
+++ marks.py
@@ -0,0 +1,11 @@
+class student:
+    def __init__(self,name,rollno,aicamarks,adcmarks):
+        self.name=name
+        self.rollno=rollno
+        self.aicamarks=aicamarks
+        self.adcmarks=adcmarks
+obj =student("kareem",22,65,62)
+print(obj.name)
+print(obj.rollno)
+print(obj.aicamarks)
+print(obj.adcmarks)
